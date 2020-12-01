b15165d5cc0acb846a3320b3b13360afda626063
Tim Peters <tim@catminion.net>
2020-12-01 17:31:28 +1030

    Add day 1 part 2

--- lib/day1.rb
+++ lib/day1.rb
@@ -1,11 +1,15 @@
 class Day1 < Base
   def part1
-    find_sums(2020).inject(:*)
+    find_sums(2020, 2).inject(:*)
   end
 
-  def find_sums(target)
-    input_to_ints.combination(2) do |a, b|
-      return [a, b] if a + b == target
+  def part2
+    find_sums(2020, 3).inject(:*)
+  end
+
+  def find_sums(target, count)
+    input_to_ints.combination(count) do |values|
+      return values if values.inject(:+) == target
     end
     nil
   end
--- test/day1_test.rb
+++ test/day1_test.rb
@@ -1,17 +1,26 @@
 require "day1"
 
 class Day1Test < Minitest::Test
+  TEST_INPUT = <<~END
+    1721
+    979
+    366
+    299
+    675
+    1456
+  END
+
   def test_part1
     d1 = Day1.new
-    d1.test_input <<~END
-      1721
-      979
-      366
-      299
-      675
-      1456
-    END
-    assert_equal [1721, 299], d1.find_sums(2020)
+    d1.test_input TEST_INPUT
+    assert_equal [1721, 299], d1.find_sums(2020, 2)
     assert_equal 514579, d1.part1
+  end
+
+  def test_part2
+    d1 = Day1.new
+    d1.test_input TEST_INPUT
+    assert_equal [979, 366, 675], d1.find_sums(2020, 3)
+    assert_equal 241861950, d1.part2
   end
 end
