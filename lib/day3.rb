fa9492a3471931b94794dfefa1fe2df781463554
Tim Peters <tim@catminion.net>
2020-12-03 17:51:51 +1030

    Add day 3 part 1

---
+++ lib/day3.rb
@@ -0,0 +1,37 @@
+class Day3 < Base
+  def part1
+    count_trees(right: 3, down: 1)
+  end
+
+  def grid
+    @grid ||= parse_grid
+  end
+
+  def width
+    @width ||= grid[0].size
+  end
+
+  def at(row, col)
+    grid.dig(row, col % width)
+  end
+
+  def count_trees(right:, down:)
+    row = col = trees = 0
+    while here = at(row, col)
+      trees += 1 if here == TREE
+      row += down
+      col += right
+    end
+    trees
+  end
+
+  TREE = "#"
+  OPEN = "."
+
+  def parse_grid
+    raw_input.each_line.map do |row|
+      @width ||= row.chomp.length
+      row.chomp.each_char.to_a
+    end
+  end
+end
---
+++ test/day3_test.rb
@@ -0,0 +1,25 @@
+require "day3"
+
+class Day3Test < Minitest::Test
+  TEST_INPUT = <<~END
+    ..##.......
+    #...#...#..
+    .#....#..#.
+    ..#.#...#.#
+    .#...##..#.
+    ..#.##.....
+    .#.#.#....#
+    .#........#
+    #.##...#...
+    #...##....#
+    .#..#...#.#
+  END
+
+  def test_part1
+    d3 = Day3.new(TEST_INPUT)
+    assert_equal ".", d3.at(0, 0)
+    assert_equal "#", d3.at(0, 2)
+    assert_equal 7, d3.count_trees(right: 3, down: 1)
+    assert_equal 7, d3.part1
+  end
+end
