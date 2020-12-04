a39486e3b02213be0661562a339623c8be007cc2
Tim Peters <tim@catminion.net>
2020-12-04 18:34:45 +1030

    Add day 4 part 2

--- lib/day4.rb
+++ lib/day4.rb
@@ -3,6 +3,10 @@ class Day4 < Base
     passports.count(&:valid?)
   end
 
+  def part2
+    passports.count(&:strict_valid?)
+  end
+
   def passports
     @passports ||= parse_passports
   end
@@ -12,9 +16,27 @@ class Day4 < Base
       @fields = fields
     end
 
+    attr_reader :fields
+
     def valid?
-      missing = %w[byr iyr eyr hgt hcl ecl pid cid] - @fields.keys
+      missing = %w[byr iyr eyr hgt hcl ecl pid cid] - fields.keys
       missing.none? || missing == ["cid"]
+    end
+
+    def strict_valid?
+      valid? &&
+        fields["byr"].between?("1920", "2002") &&
+        fields["iyr"].between?("2010", "2020") &&
+        fields["eyr"].between?("2020", "2030") &&
+        valid_height? &&
+        fields["hcl"].match?(/^#\h{6}$/) &&
+        %w[amb blu brn gry grn hzl oth].include?(fields["ecl"]) &&
+        fields["pid"].match?(/^\d{9}$/)
+    end
+
+    def valid_height?
+      (fields["hgt"].end_with?("cm") && fields["hgt"].to_i.between?(150, 193)) ||
+        (fields["hgt"].end_with?("in") && fields["hgt"].to_i.between?(59, 76))
     end
   end
 
--- test/day4_test.rb
+++ test/day4_test.rb
@@ -17,6 +17,37 @@ class Day4Test < Minitest::Test
     iyr:2011 ecl:brn hgt:59in
   END
 
+  TEST_INVALID = <<~END
+    eyr:1972 cid:100
+    hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+    iyr:2019
+    hcl:#602927 eyr:1967 hgt:170cm
+    ecl:grn pid:012533040 byr:1946
+
+    hcl:dab227 iyr:2012
+    ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+    hgt:59cm ecl:zzz
+    eyr:2038 hcl:74454a iyr:2023
+    pid:3556412378 byr:2007
+  END
+
+  TEST_VALID = <<~END
+    pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+    hcl:#623a2f
+
+    eyr:2029 ecl:blu cid:129 byr:1989
+    iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+    hcl:#888785
+    hgt:164cm byr:2001 iyr:2015 cid:88
+    pid:545766238 ecl:hzl
+    eyr:2022
+
+    iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
+  END
+
   def test_part1
     d4 = Day4.new(TEST_INPUT)
     assert_equal 4, d4.passports.size
@@ -25,5 +56,14 @@ class Day4Test < Minitest::Test
     assert d4.passports[2].valid?
     refute d4.passports[3].valid?
     assert_equal 2, d4.part1
+  end
+
+  def test_part2
+    d4 = Day4.new(TEST_INVALID)
+    assert_equal [false] * 4, d4.passports.map(&:strict_valid?)
+    assert_equal 0, d4.part2
+    d4 = Day4.new(TEST_VALID)
+    assert_equal [true] * 4, d4.passports.map(&:strict_valid?)
+    assert_equal 4, d4.part2
   end
 end
