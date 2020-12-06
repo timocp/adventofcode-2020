3b70055d334fa1a66b553f0b294168274172c98a
Tim Peters <tim@catminion.net>
2020-12-06 16:56:20 +1030

    Add day 6 part 2

--- lib/day6.rb
+++ lib/day6.rb
@@ -2,4 +2,14 @@ class Day6 < Base
   def part1
     raw_input.split("\n\n").map { |answers| answers.delete("\n").each_char.uniq.size }.sum
   end
+
+  def part2
+    raw_input.split("\n\n").map do |answers|
+      counter = Hash.new(0)
+      answers.split.each do |answer|
+        answer.split("").each { |q| counter[q] += 1 }
+      end
+      counter.values.count { |v| v == answers.lines.size }
+    end.sum
+  end
 end
--- test/day6_test.rb
+++ test/day6_test.rb
@@ -22,4 +22,8 @@ class Day6Test < Minitest::Test
   def test_part1
     assert_equal 11, Day6.new(TEST_INPUT).part1
   end
+
+  def test_part2
+    assert_equal 6, Day6.new(TEST_INPUT).part2
+  end
 end
