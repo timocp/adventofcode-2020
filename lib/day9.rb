61e8539f6c10d70490bd05e14a6dc8b1e41ff04f
Tim Peters <tim@catminion.net>
2020-12-10 17:10:28 +1030

    Add day 9 part 2

--- lib/day9.rb
+++ lib/day9.rb
@@ -4,6 +4,11 @@ class Day9 < Base
     first_invalid
   end
 
+  def part2
+    @preamble_size ||= 25
+    find_weakness.minmax.sum
+  end
+
   attr_accessor :preamble_size
 
   # Returns the first number after the preamble which is NOT the sum of any
@@ -12,5 +17,20 @@ class Day9 < Base
     input_to_ints.each_cons(preamble_size + 1).detect do |slice|
       !(slice[0, preamble_size].combination(2).detect { |pair| pair.sum == slice[-1] })
     end&.last
+  end
+
+  def find_weakness
+    invalid = first_invalid
+    input_to_ints.each.with_index do |num, low|
+      high = low + 1
+      sum = num + input_to_ints[high]
+      while sum <= invalid
+        return input_to_ints[low..high] if sum == invalid
+
+        high += 1
+        sum += input_to_ints[high]
+      end
+    end
+    nil
   end
 end
--- test/day9_test.rb
+++ test/day9_test.rb
@@ -29,4 +29,11 @@ class Day9Test < Minitest::Test
     d9.preamble_size = 5
     assert_equal 127, d9.part1
   end
+
+  def test_part2
+    d9 = Day9.new(TEST_INPUT)
+    d9.preamble_size = 5
+    assert_equal [15, 25, 47, 40], d9.find_weakness
+    assert_equal 62, d9.part2
+  end
 end
