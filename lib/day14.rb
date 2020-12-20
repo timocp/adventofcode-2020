210caee9bc3f5afc9fe1ae4dfac685d71a9f2721
Tim Peters <tim@catminion.net>
2020-12-20 19:50:55 +1030

    Add day 14 part 2

--- lib/day14.rb
+++ lib/day14.rb
@@ -7,6 +7,12 @@ class Day14 < Base
     @mem.values.sum
   end
 
+  def part2
+    @mem = Hash.new(0)
+    parse2
+    @mem.values.sum
+  end
+
   def mem(m)
     @mem[m]
   end
@@ -18,6 +24,29 @@ class Day14 < Base
         @mask1 = m[1].tr("X", "0").to_i(2)
       elsif (m = line.match(/^mem\[(\d+)\] = (\d+)$/))
         @mem[m[1].to_i] = (m[2].to_i | @mask1) & ~@mask0
+      else
+        raise "parse error: #{line.inspect}"
+      end
+    end
+  end
+
+  def parse2
+    raw_input.each_line do |line|
+      if (m = line.match(/^mask = ([X01]{36})$/))
+        @mask1 = m[1].tr("X", "0").to_i(2) # this mask changes bits to ones
+        @xbits = m[1].each_char.with_index.select { |c, _i| c == "X" }.map { |_c, i| 35 - i }
+      elsif (m = line.match(/^mem\[(\d+)\] = (\d+)$/))
+        (2**@xbits.size).times.each do |n|
+          address = m[1].to_i | @mask1
+          @xbits.each.with_index do |x, i|
+            address = if n & 2**i > 0
+                        address | (2**x) # forced to be 1 this iteration
+                      else
+                        address & ~(2**x) # forced to be 0 this iteration
+                      end
+          end
+          @mem[address] = m[2].to_i
+        end
       else
         raise "parse error: #{line.inspect}"
       end
--- test/day14_test.rb
+++ test/day14_test.rb
@@ -14,4 +14,24 @@ class Day14Test < Minitest::Test
     assert_equal 101, d14.mem(7)
     assert_equal 64, d14.mem(8)
   end
+
+  def test_part2
+    d14 = Day14.new(<<~TEST)
+      mask = 000000000000000000000000000000X1001X
+      mem[42] = 100
+      mask = 00000000000000000000000000000000X0XX
+      mem[26] = 1
+    TEST
+    assert_equal 208, d14.part2
+    assert_equal 1, d14.mem(16)
+    assert_equal 1, d14.mem(17)
+    assert_equal 1, d14.mem(18)
+    assert_equal 1, d14.mem(19)
+    assert_equal 1, d14.mem(24)
+    assert_equal 1, d14.mem(25)
+    assert_equal 1, d14.mem(26)
+    assert_equal 1, d14.mem(27)
+    assert_equal 100, d14.mem(58)
+    assert_equal 100, d14.mem(59)
+  end
 end
