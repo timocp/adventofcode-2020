ba90fd2908c4879411c5a78adecc85c0e64f380b
Tim Peters <tim@catminion.net>
2020-12-20 18:44:47 +1030

    Add day 13 part 1

--- lib/day13.rb
+++ lib/day13.rb
@@ -4,16 +4,43 @@ class Day13 < Base
     earliest_bus.inject(&:*)
   end
 
+  def part2
+    parse
+    crt(*buses.each_with_index.select { |bus, _i| bus }.map { |bus, i| [bus, bus - i] }.transpose)
+  end
+
+  # https://brilliant.org/wiki/chinese-remainder-theorem/
+  # https://rosettacode.org/wiki/Chinese_remainder_theorem
+  def crt(primes, remainders)
+    max = primes.inject(&:*)
+    x = primes.zip(remainders).map do |n, a|
+      _, s = euclid(n, max / n)
+      a * s * max / n
+    end.sum
+    x % max
+  end
+
+  def euclid(m, n)
+    if m % n == 0
+      [0, 1]
+    else
+      rs = euclid(n % m, m)
+      r = rs[1] - rs[0] * (n / m)
+      s = rs[0]
+      [r, s]
+    end
+  end
+
   def parse
     lines = raw_input.split("\n")
     @earliest_timestamp = lines[0].to_i
-    @buses = lines[1].split(",").reject { |bus| bus == "x" }.map(&:to_i)
+    @buses = lines[1].split(",").map { |bus| bus == "x" ? nil : bus.to_i }
   end
 
   attr_reader :earliest_timestamp, :buses
 
   # returns [bus_number, wait mins]
   def earliest_bus
-    buses.map { |bus| [bus, bus - (earliest_timestamp % bus)] }.min_by(&:last)
+    buses.compact.map { |bus| [bus, bus - (earliest_timestamp % bus)] }.min_by(&:last)
   end
 end
--- test/day13_test.rb
+++ test/day13_test.rb
@@ -12,4 +12,13 @@ class Day13Test < Minitest::Test
     assert_equal [59, 5], d13.earliest_bus
     assert_equal 59 * 5, d13.part1
   end
+
+  def test_part2
+    assert_equal 1068781, Day13.new(TEST_INPUT).part2
+    assert_equal 3417, Day13.new("\n17,x,13,19\n").part2
+    assert_equal 754018, Day13.new("\n67,7,59,61\n").part2
+    assert_equal 779210, Day13.new("\n67,x,7,59,61\n").part2
+    assert_equal 1261476, Day13.new("\n67,7,x,59,61\n").part2
+    assert_equal 1202161486, Day13.new("\n1789,37,47,1889\n").part2
+  end
 end
