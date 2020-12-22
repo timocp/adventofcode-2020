e3ff36cd22081c13d98f4e7d472fb1f1b212475a
Tim Peters <tim@catminion.net>
2020-12-22 21:06:53 +1030

    Add day 15 part 2

--- lib/day15.rb
+++ lib/day15.rb
@@ -1,17 +1,19 @@
 class Day15 < Base
   def part1
-    play.take(2020).last
+    play(2020)
   end
 
-  def play
-    return to_enum(:play) unless block_given?
+  def part2
+    play(30_000_000)
+  end
 
+  def play(stop)
     input = raw_input.split(",").map(&:to_i)
 
     most_recent = nil
     seen = {} # array of last 2 rounds spoken
 
-    (1..).each do |round|
+    (1..stop).each do |round|
       output = if (i = input.shift)
                  i
                elsif seen[most_recent].size == 1
@@ -19,7 +21,6 @@ class Day15 < Base
                else
                  seen[most_recent][1] - seen[most_recent][0]
                end
-      yield output
       seen[output] = if seen.key?(output)
                        [seen[output].last, round]
                      else
@@ -27,5 +28,6 @@ class Day15 < Base
                      end
       most_recent = output
     end
+    most_recent
   end
 end
--- test/day15_test.rb
+++ test/day15_test.rb
@@ -2,7 +2,16 @@ require "day15"
 
 class Day15Test < Minitest::Test
   def test_part1
-    assert_equal [0, 3, 6, 0, 3, 3, 1, 0, 4, 0], Day15.new("0,3,6").play.take(10)
+    assert_equal 0, Day15.new("0,3,6").play(1)
+    assert_equal 3, Day15.new("0,3,6").play(2)
+    assert_equal 6, Day15.new("0,3,6").play(3)
+    assert_equal 0, Day15.new("0,3,6").play(4)
+    assert_equal 3, Day15.new("0,3,6").play(5)
+    assert_equal 3, Day15.new("0,3,6").play(6)
+    assert_equal 1, Day15.new("0,3,6").play(7)
+    assert_equal 0, Day15.new("0,3,6").play(8)
+    assert_equal 4, Day15.new("0,3,6").play(9)
+    assert_equal 0, Day15.new("0,3,6").play(10)
     assert_equal 436, Day15.new("0,3,6").part1
     assert_equal 1, Day15.new("1,3,2").part1
     assert_equal 10, Day15.new("2,1,3").part1
@@ -10,5 +19,16 @@ class Day15Test < Minitest::Test
     assert_equal 78, Day15.new("2,3,1").part1
     assert_equal 438, Day15.new("3,2,1").part1
     assert_equal 1836, Day15.new("3,1,2").part1
+  end
+
+  def test_part2
+    # passing but SLOW
+    # assert_equal 175594, Day15.new("0,3,6").part2
+    # assert_equal 2578, Day15.new("1,3,2").part2
+    # assert_equal 3544142, Day15.new("2,1,3").part2
+    # assert_equal 261214, Day15.new("1,2,3").part2
+    # assert_equal 6895259, Day15.new("2,3,1").part2
+    # assert_equal 18, Day15.new("3,2,1").part2
+    # assert_equal 362, Day15.new("3,1,2").part2
   end
 end
